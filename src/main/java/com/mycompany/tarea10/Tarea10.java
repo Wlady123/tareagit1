48d036be737999258832cb572990bc8280dc2dd5
Wlady123 <dw.canart@uea.edu.ec>
2025-08-20 16:51:07 -0500

    mi primer subida

---
+++ src/main/java/com/mycompany/tarea10/Tarea10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea10;
+
+/**
+ *
+ * @author DANIEL
+ */
+public class Tarea10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
