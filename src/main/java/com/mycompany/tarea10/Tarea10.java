d2dde654457b8dc6dadb3dc3419e98f38d2468a2
Wlady123 <dw.canart@uea.edu.ec>
2025-08-21 10:36:55 -0500

    actualice el mensaje

--- src/main/java/com/mycompany/tarea10/Tarea10.java
+++ src/main/java/com/mycompany/tarea10/Tarea10.java
@@ -11,6 +11,6 @@ package com.mycompany.tarea10;
 public class Tarea10 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Wlady!");
     }
 }
